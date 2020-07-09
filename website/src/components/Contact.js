af7a807893bdfd437f315b30236e400dc762535c
Amanda Escaba <amandaescaba@gmail.com>
2020-07-09 18:37:18 -0400

    added brands icons

--- website/src/components/Contact.js
+++ website/src/components/Contact.js
@@ -2,23 +2,51 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 
 class Contact extends React.Component {
     constructor(props) {
         super(props);
     }
-    
+
 
     render() {
         return (
-        <div className="container">
-        <h1>Contact</h1>
-        <p>
-        <FontAwesomeIcon icon={faEnvelopeOpenText} />
-        </p>
-        </div>
+            <div className="container">
+                <h1>Contact</h1>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faEnvelopeOpenText} />
+                        Email me at <a href="mailto:amandaescaba@gmail.com">amandaescaba@gmail.com</a>
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faLinkedin} />
+                        Email me at <a href="mailto:amandaescaba@gmail.com">amandaescaba@gmail.com</a>
+                    </li>
+                </ul>
+            </div >
         )
     }
 }
 
 export default Contact
+
+{/* <div class="main col-md-9 col-lg-10">
+<div class="row">
+  <div class="heading">
+    <h3>contact me</h3>
+  </div>
+</div>
+<div class="row">
+    <ul>
+        <li class="contact-item">
+          <a class="contact-link" href="#">email</a>
+        </li>
+        <li class="contact-item">
+          <a class="contact-link" href="#">linkedin</a>
+        </li>
+      </ul>
+</div>
+</div> */}
