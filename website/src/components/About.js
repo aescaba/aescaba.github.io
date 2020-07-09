e82a6ced312c0fdacdba41c8cc0ef5c979b9723d
Amanda Escaba <amandaescaba@gmail.com>
2020-07-09 19:06:24 -0400

    updated headshot and about formatting

--- website/src/components/About.js
+++ website/src/components/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import Headshot from './../assets/headshot.jpg'
+
 class About extends React.Component {
     constructor(props) {
         super(props);
@@ -8,11 +10,20 @@ class About extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <h1>About Me</h1>
-                <p>I grew up in the Hudson Valley, New York, and spent the last five years going to Northeastern University in Boston studying computer science and interaction design.</p>
-                <p>I'm interested in designing compelling, useful, accessible, and beautiful user-focused experiences. Outside of work, I like to find myself consuming or creating art and media&mdash;usually through drawing, painting, or making too many Spotify playlists.</p>
-                <p>I'm currently seeking job opportunities in New York City!</p>
+            <div className="main">
+                <div className="row">
+                    <h3 className="heading">About Me</h3>
+                </div>
+                <div className="about row">
+                    <div className="about-text col">
+                        <p>I grew up in the Hudson Valley, New York, and spent the last five years going to Northeastern University in Boston studying computer science and interaction design.</p>
+                        <p>I'm interested in designing compelling, useful, accessible, and beautiful user-focused experiences. Outside of work, I like to find myself consuming or creating art and media&mdash;usually through drawing, painting, or making too many Spotify playlists.</p>
+                        <p>I'm currently seeking job opportunities in New York City!</p>
+                    </div>
+                    <div className="about-img-container col">
+                        <img src={Headshot} class="about-img img-fluid" alt="Insert image description here" />
+                    </div>
+                </div>
             </div>
         )
     }
